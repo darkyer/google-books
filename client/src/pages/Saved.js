0df37d2edc3eebeaeb78861253b25ae3118653a9
Ernesto Solares <ernesto.solares.guerrero@gmail.com>
2019-07-19 10:30:15 -0500

    basic folder structure and some code

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+
+class Saved extends Component {
+    state = {
+        savedBooks: []
+    };
+
+    //when this component mounts, grab all books that were save to the database 
+    componentDidMount() {
+        API.getBooks()
+            .then(res => this.setState({ savedBooks: res.data }))
+            .catch(err => console.log(err))
+    }
+
+    //function to remove book by id
+    handleDeleteButton = id => {
+        API.deleteBook(id)
+            .then(res => this.componentDidMount())
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <Container fluid className="container">
+                <Jumbotron />
+                <Container>
+                    <SavedResult savedBooks={this.state.savedBooks} handleDeleteButton={this.handleDeleteButton} />
+                </Container>
+            </Container>
+        )
+    }
+}
+
+export default Saved;
